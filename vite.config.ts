5ca7b25094f1fe5d4998037a253deebd94c3c182
KisuraWSP <kisura2003@gmail.com>
2025-12-06 11:00:35 +0530

    defer

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // REPLACE 'kefir-lang' WITH YOUR ACTUAL REPO NAME
+  // Example: If your repo is https://github.com/user/my-compiler
+  // Then use base: '/my-compiler/'
+  base: 'https://github.com/devgithub-sl/kefirlang.git' 
+})
